ea43a480a3dc3231a415ac9bf95b3b71471a8bf4
lgh <lghwuwangchuxin@163.com>
2019-04-19 16:09:00 +0800

    添加字段注释

--- cloud-service/api-model/src/main/java/com/cloud/model/user/WechatUserInfo.java
+++ cloud-service/api-model/src/main/java/com/cloud/model/user/WechatUserInfo.java
@@ -16,18 +16,57 @@ import java.util.Date;
 public class WechatUserInfo implements Serializable {
 
     private static final long serialVersionUID = 6750304307961875043L;
-
+    /**
+     * 主键
+     */
     private Long id;
+    /**
+     * 普通用户的标识，对当前开发者帐号唯一
+     */
     private String openid;
+    /**
+     * 用户统一标识。针对一个微信开放平台帐号下的应用，同一用户的unionid  是唯一的
+     */
     private String unionid;
+    /**
+     * 用户主键
+     */
     private Long userId;
+    /**
+     * app
+     */
     private String app;
+    /**
+     * 普通用户昵称
+     */
     private String nickname;
+    /**
+     * 普通用户性别，1为男性，2为女性
+     */
     private String sex;
+    /**
+     * 普通用户个人资料填写的省份
+     */
     private String province;
+    /**
+     * 普通用户个人资料填写的城市
+     */
     private String city;
+    /**
+     * 国家，如中国为CN
+     */
     private String country;
+    /**
+     * 用户头像，最后一个数值代表正方形头像大小（
+     * 有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空
+     */
     private String headimgurl;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 修改时间
+     */
     private Date updateTime;
 }

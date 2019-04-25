fc0986cda120086de461c62cc2e7eb20ce311123
lghwuwangchuxin <lghwuwangchuxin@163.com>
2019-04-25 16:14:40 +0800

    添加上传docker地址

--- cloud-service/organization-center/src/test/java/com/cloud/organization/OrganizationTest.java
+++ cloud-service/organization-center/src/test/java/com/cloud/organization/OrganizationTest.java
@@ -1,5 +1,7 @@
 package com.cloud.organization;
 
+import com.cloud.model.organization.CloudOrgModel;
+import com.cloud.organization.service.ICloudOrgService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class OrganizationTest {
-
-
+    @Autowired
+    private ICloudOrgService orgservice;
+@Test
+public void  inserOrg(){
+    /*CloudOrgModel org=new CloudOrgModel();
+    org.setOrgCode("1");
+    orgservice.insertOrg(org);*/
+    System.out.println("sdfaf");
+}
 
 }

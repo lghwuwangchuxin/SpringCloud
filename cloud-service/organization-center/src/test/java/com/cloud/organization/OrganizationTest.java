7dceff699ceaa3057e1421b56d40139168ed1b1a
lghwuwangchuxin <lghwuwangchuxin@163.com>
2019-04-25 16:31:08 +0800

    删除

--- cloud-service/organization-center/src/test/java/com/cloud/organization/OrganizationTest.java
+++ cloud-service/organization-center/src/test/java/com/cloud/organization/OrganizationTest.java
@@ -1,26 +1,18 @@
 package com.cloud.organization;
 
-import com.cloud.model.organization.CloudOrgModel;
-import com.cloud.organization.service.ICloudOrgService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class OrganizationTest {
-    @Autowired
-    private ICloudOrgService orgservice;
+
 @Test
 public void  inserOrg(){
-    /*CloudOrgModel org=new CloudOrgModel();
-    org.setOrgCode("1");
-    orgservice.insertOrg(org);*/
-    System.out.println("sdfaf");
+
 }
 
 }

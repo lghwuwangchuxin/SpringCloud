4faf2f68705d3f5e4e46cd1ed711a831c74491b9
lghwuwangchuxin <lghwuwangchuxin@163.com>
2019-04-28 15:31:02 +0800

    在组织工程添加测试

---
+++ cloud-service/organization-center/src/main/java/com/cloud/organization/service/CloudOrgService.java
@@ -0,0 +1,16 @@
+package com.cloud.organization.service;
+
+import com.cloud.organization.model.CloudOrg;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lgh
+ * @since 2019-04-28
+ */
+public interface CloudOrgService extends IService<CloudOrg> {
+
+}
---
+++ cloud-service/organization-center/src/main/java/com/cloud/organization/service/impl/CloudOrgServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cloud.organization.service.impl;
+
+import com.cloud.organization.model.CloudOrg;
+import com.cloud.organization.mapper.CloudOrgDao;
+import com.cloud.organization.service.CloudOrgService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author lgh
+ * @since 2019-04-28
+ */
+@Service
+public class CloudOrgServiceImpl extends ServiceImpl<CloudOrgDao, CloudOrg> implements CloudOrgService {
+
+}
